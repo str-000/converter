6db1baf8a8a9651f2af3b5dda264d656c9a6bec5
Łukasz Strama <strama.lukasz54@gmail.com>
2022-04-11 19:14:19 +0200

    states

--- src/Card.js
+++ src/Card.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import InputForm from './InputForm'
 
-export default function Card() {
-    return (
-        <div>
-            <div className='card-buttons'>
-                <button className='card-button'>
-                    <span>długość</span>
-                </button>
-                <button className='card-button'>
-                    <span>waga</span>
-                </button>
-                <button className='card-button'>
-                    <span>prędkość</span>
-                </button>
-                <button className='card-button'>
-                    <span>powierzchnia</span>
-                </button>
-            </div>
+export default class Card extends React.Component {
 
-            <div className='input-container'>
-                <InputForm/>
+    constructor(props) {
+        super(props)
+        this.state = {selectedCard: 1}
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='card-buttons'>
+                    <button className={this.state.selectedCard === 1 ? 'card-button-active' : 'card-button'}>
+                        <span>długość</span>
+                    </button>
+                    <button className={this.state.selectedCard === 2 ? 'card-button-active' : 'card-button'}>
+                        <span>waga</span>
+                    </button>
+                    <button className={this.state.selectedCard === 3 ? 'card-button-active' : 'card-button'}>
+                        <span>prędkość</span>
+                    </button>
+                    <button className={this.state.selectedCard === 4 ? 'card-button-active' : 'card-button'}>
+                        <span>powierzchnia</span>
+                    </button>
+                </div>
+
+                <div className='input-container'>
+                    <InputForm />
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
--- src/InputForm.js
+++ src/InputForm.js
@@ -1,28 +1,36 @@
 import React from 'react'
 import { Form, Row, Col } from 'react-bootstrap';
 
-export default function InputForm() {
-    return (
-        <Form>
-            <Row>
-                <Col>
-                    <Form.Control />
-                    <Form.Label>Jednostka</Form.Label>
-                    <Form.Select multiple defaultValue="Choose...">
-                        <option>metry [m]</option>
-                        <option>kilometry [km]</option>
-                    </Form.Select>
-                </Col>
-                =
-                <Col>
-                    <Form.Control />
-                    <Form.Label>Jednostka</Form.Label>
-                    <Form.Select multiple defaultValue="Choose...">
-                        <option>metry [m]</option>
-                        <option>kilometry [km]</option>
-                    </Form.Select>
-                </Col>
-            </Row>
-        </Form>
-    )
+export default class InputForm extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {inputValue: ''}
+    }
+
+    render() {
+        return (
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Control />
+                        <Form.Label>Jednostka</Form.Label>
+                        <Form.Select multiple defaultValue="Choose...">
+                            <option>metry [m]</option>
+                            <option>kilometry [km]</option>
+                        </Form.Select>
+                    </Col>
+                    =
+                    <Col>
+                        <Form.Control />
+                        <Form.Label>Jednostka</Form.Label>
+                        <Form.Select multiple defaultValue="Choose...">
+                            <option>metry [m]</option>
+                            <option>kilometry [km]</option>
+                        </Form.Select>
+                    </Col>
+                </Row>
+            </Form>
+        )
+    }
 }
