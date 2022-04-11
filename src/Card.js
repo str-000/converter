d7ae04077dfbd4dc37c22a02237ca7574d9b463a
Łukasz Strama <strama.lukasz54@gmail.com>
2022-04-11 21:06:33 +0200

    1

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import InputForm from './InputForm'
 
+<<<<<<< HEAD
 export default class Card extends React.Component {
 
     constructor(props) {
@@ -29,6 +30,48 @@ export default class Card extends React.Component {
                 <div className='input-container'>
                     <InputForm />
                 </div>
+=======
+export default function Card() {
+    return (
+        <div>
+            <div className='card-buttons'>
+                <button className='card-button'>
+                    <span>długość</span>
+                </button>
+                <button className='card-button'>
+                    <span>waga</span>
+                </button>
+                <button className='card-button'>
+                    <span>prędkość</span>
+                </button>
+                <button className='card-button'>
+                    <span>powierzchnia</span>
+                </button>
+            </div>
+
+            <div className='input-container'>
+                <Form>
+                    <Row>
+                        <Col>
+                            <Form.Control />
+                            <Form.Label>Jednostka</Form.Label>
+                            <Form.Select multiple defaultValue="Choose...">
+                                <option>metry [m]</option>
+                                <option>kilometry [km]</option>
+                            </Form.Select>
+                        </Col>
+                        =
+                        <Col>
+                            <Form.Control />
+                            <Form.Label>Jednostka</Form.Label>
+                            <Form.Select multiple defaultValue="Choose...">
+                                <option>metry [m]</option>
+                                <option>kilometry [km]</option>
+                            </Form.Select>
+                        </Col>
+                    </Row>
+                </Form>
+>>>>>>> parent of 532f8ab (1)
             </div>
         )
     }
